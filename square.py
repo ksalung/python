527e9e4ff9e5256a3f3a2d647ea6d06aa3eac2e1
leejy <misail0118@naver.com>
2025-06-27 14:44:46 +0900

    practice2days

---
+++ square.py
@@ -0,0 +1,21 @@
+#제곱
+a = int(input())
+i = 0
+while i < a:
+    i = i + 1
+    print(a)
+
+#제곱표
+num = int(input())
+n = 1
+while n <= num:
+    print(n, n*n)
+    n =  n + 1
+
+#얌체공
+height = 100.0
+n = 1
+while n <= 10:
+    height = height *(3/5)
+    print(round(height, 4))
+    n = n + 1
